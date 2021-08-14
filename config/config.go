96917554cf685e835d896fe3acb5d7e100c02b4c
taiki yamashita <taiki0808ty@gmail.com>
2021-08-15 00:12:57 +0900

    feat: config.ini設定

--- config/config.go
+++ config/config.go
@@ -1,1 +1,25 @@
 package config
+
+import "log"
+
+type Config struct {
+	Port      string
+	SQLDriver string
+	DbName    string
+	LogFile   string
+}
+
+var Config ConfigList
+
+func LoadConfig() {
+	cfg, err := ini.Load("config.ini")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	Config = ConfigList {
+		Port: cfg.Section("web").Key("port").MustString("8080"),
+		SQLDriver: cfg.Section("db").Key("driver").String(),
+		DbName: cfg.Section("db").Key("name").String(),
+		LogFile: cfg.Section("web").Key("logfile").String(),
+	}
+}
