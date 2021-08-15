c575a4df0a9b48c3db4cee704fb70a299818bd5c
taiki yamashita <taiki0808ty@gmail.com>
2021-08-15 18:24:20 +0900

    feat: Users table create

--- app/models/base.go
+++ app/models/base.go
@@ -1,12 +1,14 @@
 package models
 
 import (
+	"crypto/sha1"
 	"database/sql"
 	"fmt"
-	"go-todo/config"
 	"log"
+	"go-todo/config"
 
 	"github.com/google/uuid"
+
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -27,15 +29,20 @@ func init() {
 	cmdU := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
 		uuid STRING NOT NULL UNIQUE,
-		name STRING
-		email STRING
-		password STRING
+		name STRING,
+		email STRING,
+		password STRING,
 		created_at DATETIME)`, tableNameUser)
 
 	Db.Exec(cmdU)
 }
 
 func createUUID() (uuidobj uuid.UUID) {
-	uuidobj, _ := uuid.NewUUID()
+	uuidobj, _ = uuid.NewUUID()
 	return uuidobj
+}
+
+func Encrypt(plaintext string) (cryptext string) {
+	cryptext = fmt.Sprintf("%x", sha1.Sum([]byte(plaintext)))
+	return cryptext
 }
