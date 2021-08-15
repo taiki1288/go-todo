90d24980379dded21bc54b2d80ddd1d727af974a
taiki yamashita <taiki0808ty@gmail.com>
2021-08-16 00:59:16 +0900

    update: エラーハンドリング追加

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -2,10 +2,14 @@ package controllers
 
 import (
 	"html/template"
+	"log"
 	"net/http"
 )
 
 func top(w http.ResponseWriter, r *http.Request) {
-	t, _ := template.ParseFiles("app/views/templates/top.html")
-	t.Execute(w, nil)
+	t, err := template.ParseFiles("app/views/templates/top.html")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	t.Execute(w, "Hello")
 }
