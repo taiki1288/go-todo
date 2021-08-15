08b3261de8a4be06151eadae95f79b10a2693391
taiki yamashita <taiki0808ty@gmail.com>
2021-08-15 20:45:22 +0900

    feat: CreateTodo function

--- app/models/todos.go
+++ app/models/todos.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"log"
 	"time"
 )
 
@@ -9,4 +10,17 @@ type Todo struct {
 	Content string
 	UserID int
 	CreatedAt time.Time
+}
+
+func (u *User) CreateTodo(content string) (err error) {
+	cmd := `insert into todos (
+		content,
+	    user_id, 
+		created_at) values (?, ?, ?)`
+
+	_, err = Db.Exec(cmd, content, u.ID, time.Now())
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
 }
