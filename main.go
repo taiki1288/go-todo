49defaa2555208907a0383d3a3ced35029bce6b0
taiki yamashita <taiki0808ty@gmail.com>
2021-08-15 09:54:33 +0900

    feat: DB table setting

--- main.go
+++ main.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"go-todo/config"
+	"go-todo/app/models"
 )
 
 func main() {
+	/*
 	fmt.Println(config.Config.Port)
 	fmt.Println(config.Config.SQLDriver)
 	fmt.Println(config.Config.DbName)
 	fmt.Println(config.Config.LogFile)
 
 	log.Println("test")
+	*/
+
+	fmt.Println(models.Db)
 }
