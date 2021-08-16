693debdce3da8d360d1ce12acdf04d2bcc1341f4
taiki yamashita <taiki0808ty@gmail.com>
2021-08-16 19:45:00 +0900

    feat: index function

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -7,3 +7,12 @@ import (
 func top(w http.ResponseWriter, r *http.Request) {
 	generateHTML(w, "Hello", "layout", "public_navbar", "top")
 }
+
+func index(w http.ResponseWriter, r *http.Request) {
+	sess, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/", 302)
+	} else {
+		generateHTML(w, nil, "layout", "index")
+	}
+}
