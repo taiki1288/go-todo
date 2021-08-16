fef0248a2d78c37ab06d502ea3151dd77636d906
taiki yamashita <taiki0808ty@gmail.com>
2021-08-16 16:20:24 +0900

    feat: LoginFunction

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -22,6 +22,7 @@ func StartMainServer() error {
 	http.Handle("/static/", http.StripPrefix("/static/", files))
 	http.HandleFunc("/", top)
 	http.HandleFunc("/signup", signup)
+	http.HandleFunc("/login", login)
 	return http.ListenAndServe(":" + config.Config.Port, nil)
 }
 
